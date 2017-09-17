aa6c406b47fe9ec92d3897977574bef1ba8d4cd0
Calla Rudolph <callarudolph@gmail.com>
2017-09-16 17:38:35 -0700

    pass second text

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 const chorus = "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye"
 
 const chorusArray = chorus.split(', ');
+let position = 0;
 
 const initialState = {
-  chorus: chorus,
+  chorusString: chorus,
   chorusArray: chorusArray,
-  position: 0,
-  currentPhrase: chorusArray[0]
+  arrayPosition: position,
+  currentPhrase: chorusArray[position]
 }
 
 console.log(initialState);
@@ -14,7 +15,20 @@ console.log(initialState);
 // REDUCER
 
 const reducer = (state = initialState, action) => {
-  return state;
+  let newState;
+  switch (action.type) {
+    case 'NEXT_LYRIC':
+      let newPosition = state.arrayPosition + 1;
+      newState = {
+        chorusString: state.chorusString,
+        chorusArray: state.chorusArray,
+        arrayPosition: newPosition,
+        currentPhrase: state.chorusArray[newPosition]
+      }
+      return newState;
+    default:
+      return state;
+  }
 }
 
 const { expect } = window;
